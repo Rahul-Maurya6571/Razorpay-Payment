74fe63d7a8c0d246ed2d56a42fa70452f532bd0d
Rahul Maurya <rahulmaurya6571@gmail.com>
2023-03-14 16:26:19 +0530

    payment gateway

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Box, Stack } from "@chakra-ui/react"
+import Card from './Card'
+import axios from "axios";
+
+
+const Home = () => {
+
+
+    const checkoutHandler = async (amount) => {
+
+        const { data: { key } } = await axios.get("http://www.localhost:4000/api/getkey")
+
+        const { data: { order } } = await axios.post("http://localhost:4000/api/checkout", {
+            amount
+        })
+
+        const options = {
+            key,
+            amount: order.amount,
+            currency: "INR",
+            name: "Rahul Maurya",
+            description: "Payment gateway RazorPay",
+            image: "https://avatars.githubusercontent.com/u/80587770?s=40&u=6cd4094ab56794f84353074bda320e23fca58ebf&v=4",
+            order_id: order.id,
+            callback_url: "http://localhost:4000/api/paymentverification",
+            prefill: {
+                name: "Rahul Maurya",
+                email: "rahulmaurya6571@gmail.com",
+                contact: "9999999999"
+            },
+            notes: {
+                "address": "Razorpay Corporate Office"
+            },
+            theme: {
+                "color": "#121212"
+            }
+        };
+        const razor = new window.Razorpay(options);
+        razor.open();
+    }
+
+    return (
+        <Box>
+
+            <Stack h={"100vh"} alignItems="center" justifyContent="center" direction={["column", "row"]}>
+
+                <Card amount={5000} img={"https://images.unsplash.com/photo-1602080858428-57174f9431cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"} checkoutHandler={checkoutHandler} />
+                <Card amount={3000} img={"http://i1.adis.ws/i/canon/eos-r5_front_rf24-105mmf4lisusm_32c26ad194234d42b3cd9e582a21c99b"} checkoutHandler={checkoutHandler} />
+
+            </Stack>
+        </Box>
+    )
+}
+
+export default Home
